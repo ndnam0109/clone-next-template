b7e502f046080c53166a558a832830a991b16be3
Brenner <justin.brenner@forgetechnologysolutions.com>
2022-07-08 15:12:53 -0400

    nodemailer

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,6 +12,7 @@ import path from 'path';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SERVER_HOST,
   port: process.env.EMAIL_SERVER_PORT,
+  secure: true,
   auth: {
     user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD,
